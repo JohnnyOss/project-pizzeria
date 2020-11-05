74bc8c81520b21c48826ce27b0eb5df010b41d64
Jan Ossolinski <ossolinski.jan@gmail.com>
2020-11-05 21:10:18 +0000

    Add class Booking and initBooking

---
+++ src/js/components/Booking.js
@@ -0,0 +1,35 @@
+import {select, templates} from '../settings.js';
+import utils from '../utils.js';
+import AmountWidget from './AmountWidget.js';
+
+class Booking {
+  constructor(element){
+    const thisBooking = this;
+
+    thisBooking.render(element);
+    thisBooking.initWidgets();
+  }
+
+  render(element){
+    const thisBooking = this;
+    const generateHTML = templates.bookingWidget();
+
+    thisBooking.dom = {};
+    thisBooking.dom.wrapper = element;
+    thisBooking.generatedDOM = utils.createDOMFromHTML(generateHTML);
+    thisBooking.dom.wrapper.appendChild(thisBooking.generatedDOM);
+
+    thisBooking.dom.peopleAmount = thisBooking.generatedDOM.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = thisBooking.generatedDOM.querySelector(select.booking.hoursAmount);
+
+  }
+
+  initWidgets(){
+    const thisBooking = this;
+
+    thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+  }
+}
+
+export default Booking;
